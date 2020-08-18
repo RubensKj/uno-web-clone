e9ba2196535dd02a9909cc912a145681ddd1f1fe
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-08-18 19:53:59 -0300

    Add Initial React Project

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle  } from 'styled-components';
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    font: 16px 'Poppins', sans-serif;
+    background-color: #fafbff;
+    color: #333;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  button {
+    font: 16px 'Poppins', sans-serif;
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
